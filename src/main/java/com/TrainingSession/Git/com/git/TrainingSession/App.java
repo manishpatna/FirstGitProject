33dfbb532ea5bfb2199321cbe599f7a1a05b4b7d
manish kumar <manish_testing@hotmail.com>
2019-04-20 23:32:42 +0530

    commiting project to local

---
+++ src/main/java/com/TrainingSession/Git/com/git/TrainingSession/App.java
@@ -0,0 +1,13 @@
+package com.TrainingSession.Git.com.git.TrainingSession;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/com/TrainingSession/Git/com/git/TrainingSession/JavaTraining.java
@@ -0,0 +1,19 @@
+package com.TrainingSession.Git.com.git.TrainingSession;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class JavaTraining {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public static void findDuplicateCharacter(String str){
+		Map<Character, Integer> map= new HashMap<Character,Integer>();
+		
+		
+	}
+
+}
