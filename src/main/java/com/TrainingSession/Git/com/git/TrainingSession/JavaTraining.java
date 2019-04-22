d665c97e1f40670f71f216599e23b8b9b4543a72
manish kumar <manish_testing@hotmail.com>
2019-04-22 11:01:44 +0530

    added the target material to git without the jar file

--- src/main/java/com/TrainingSession/Git/com/git/TrainingSession/JavaTraining.java
+++ src/main/java/com/TrainingSession/Git/com/git/TrainingSession/JavaTraining.java
@@ -7,7 +7,7 @@ public class JavaTraining {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("Hello World");
 	}
 	
 	public static void findDuplicateCharacter(String str){
